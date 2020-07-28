e56d22c050bd1859dc3693b8bbd5b8c6f48061eb
Yves <byiringiroyves127@gmail.com>
2020-07-28 21:08:37 +0200

    removing unwanted file

--- kigalimallenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/yves/Documents/0.Workspace/# MY django projects/ecommerce_ve1/kigalimallenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
