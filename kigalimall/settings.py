7b206b5fc5c520ecf85d726e9f26ba83f013448b
Yves <byiringiroyves127@gmail.com>
2020-07-30 15:12:03 +0200

    some changes

--- kigalimall/settings.py
+++ kigalimall/settings.py
@@ -39,7 +39,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k)e4if*2t90zmxqaft_&qhy67rk6t4111o5t@-qc5t09lz1u+3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['kigalimall.com','www.kigalimall.com','139.59.139.64']
 
