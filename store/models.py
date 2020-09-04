4db78aaf64c0a9db49603ce25568f2a5ea92483c
Yves <byiringiroyves127@gmail.com>
2020-09-04 20:36:18 +0200

    updates

--- store/models.py
+++ store/models.py
@@ -186,7 +186,7 @@ class ShippingAddress(models.Model):
     city = models.CharField(max_length=200, null=False)
     state = models.CharField(max_length=200, null=False)
     country = models.CharField(max_length=200, null=False)
-    phone = models.CharField(max_length=12, null=True, blank=True)
+    phone = models.CharField(max_length=12, null=True, blank=True , default='0781607266')
     date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
