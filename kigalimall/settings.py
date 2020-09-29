104d83e34af0e0fae5d08ae9a257b7dc2ec9b6ba
Yves <byiringiroyves127@gmail.com>
2020-09-29 22:42:46 +0200

    updates

--- kigalimall/settings.py
+++ kigalimall/settings.py
@@ -114,13 +114,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'kigalimall.wsgi.application'
 
 
-# DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
-
 
 # DATABASES = {
 #     'default': {
